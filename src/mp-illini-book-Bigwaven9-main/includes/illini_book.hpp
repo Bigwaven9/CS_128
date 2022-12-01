79f871ae7a61a7e89bf3a22081574e4f49122e5e
Jiahui <104997567+Bigwaven9@users.noreply.github.com>
2022-11-30 19:48:20 -0600

    Update illini_book.hpp

--- src/mp-illini-book-Bigwaven9-main/includes/illini_book.hpp
+++ src/mp-illini-book-Bigwaven9-main/includes/illini_book.hpp
@@ -6,194 +6,195 @@
 #include <queue>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "utilities.hpp"
 
-using namespace utilities;
 using namespace std;
-
-class Graph {
-private:
-  int idx_ = 0;
-  map<int, int> h_;
-  map<int, int> e_;
-  map<int, int> ne_;
-  map<int, string> w_;
-
-public:
-  Graph() = default;
-  void Add(int a, int b, string& c) {
-    e_[++idx_] = b, w_[idx_] = c, ne_[idx_] = h_[a], h_[a] = idx_;
-  }
-  void Add(int a) { h_[a] = 0; }
-
-  int Connected(int a, int b) {
-    int cnt = 0;
-    queue<int> q;
-    q.push(a);
-    set<int> st;
-    st.insert(a);
-    while (!q.empty()) {
-      for (unsigned x = q.size(); x != 0; --x) {
-        int t = q.front();
-        q.pop();
-        if (t == b) return cnt;
-        for (int i = h_[t]; i != 0; i = ne_[i]) {
-          int j = e_[i];
-          if (st.count(j) == 0) {
-            q.push(j);
-            st.insert(j);
-          }
-        }
-      }
-      cnt++;
-    }
-    return -1;
-  }
-
-  int Connected(int a, int b, const string& c) {
-    int cnt = 0;
-    queue<int> q;
-    q.push(a);
-    set<int> st;
-    st.insert(a);
-    while (!q.empty()) {
-      for (unsigned x = q.size(); x != 0; --x) {
-        int t = q.front();
-        q.pop();
-        if (t == b) return cnt;
-        for (int i = h_[t]; i != 0; i = ne_[i]) {
-          int j = e_[i];
-          if (st.count(j) == 0 && w_[i] == c) {
-            q.push(j);
-            st.insert(j);
-          }
-        }
-      }
-      cnt++;
-    }
-    return -1;
-  }
-
-  vector<int> GetSteps(int a, int n) {
-    queue<int> q;
-    q.push(a);
-    set<int> st;
-    st.insert(a);
-    while (!q.empty() && n-- != 0) {
-      for (unsigned x = q.size(); x != 0; --x) {
-        int t = q.front();
-        q.pop();
-        for (int i = h_[t]; i != 0; i = ne_[i]) {
-          int j = e_[i];
-          if (st.count(j) == 0) {
-            q.push(j);
-            st.insert(j);
-          }
-        }
-      }
-    }
-    vector<int> v;
-    while (!q.empty()) v.push_back(q.front()), q.pop();
-    return v;
-  }
-
-  size_t CountGroups() {
-    size_t cnt = 0;
-    set<int> st;
-    for (auto& [x, y] : h_) {
-      if (st.count(x) == 0) {
-        queue<int> q;
-        q.push(x);
-        st.insert(x);
-        while (!q.empty()) {
-          int t = q.front();
-          q.pop();
-          for (int i = h_[t]; i != 0; i = ne_[i]) {
-            int j = e_[i];
-            if (st.count(j) == 0) {
-              q.push(j);
-              st.insert(j);
-            }
-          }
-        }
-        cnt++;
-      }
-    }
-    return cnt;
-  }
-
-  size_t CountGroups(const string& relationship) {
-    size_t cnt = 0;
-    set<int> st;
-    for (auto& [x, y] : h_) {
-      if (st.count(x) == 0) {
-        queue<int> q;
-        q.push(x);
-        st.insert(x);
-        while (!q.empty()) {
-          int t = q.front();
-          q.pop();
-          for (int i = h_[t]; i != 0; i = ne_[i]) {
-            int j = e_[i];
-            if (st.count(j) == 0 && w_[i] == relationship) {
-              q.push(j);
-              st.insert(j);
-            }
-          }
-        }
-        cnt++;
-      }
-    }
-    return cnt;
-  }
-
-  size_t CountGroups(const vector<string>& relationships_) {
-    set<string> relationships;
-    for (const auto& i : relationships_) relationships.insert(i);
-    size_t cnt = 0;
-    set<int> st;
-    for (auto& [x, y] : h_) {
-      if (st.count(x) == 0) {
-        queue<int> q;
-        q.push(x);
-        st.insert(x);
-        while (!q.empty()) {
-          int t = q.front();
-          q.pop();
-          for (int i = h_[t]; i != 0; i = ne_[i]) {
-            int j = e_[i];
-            if (st.count(j) == 0 && relationships.count(w_[i]) != 0) {
-              q.push(j);
-              st.insert(j);
-            }
-          }
-        }
-        cnt++;
-      }
-    }
-    return cnt;
-  }
-};
+using namespace utilities;
 
 class IlliniBook {
-private:
-  Graph g_;
-
 public:
   IlliniBook(const std::string& people_fpath,
-             const std::string& relations_fpath);
+             const std::string& relations_fpath) {
+    ifstream people{people_fpath}, relations{relations_fpath};
+    for (string line; getline(people, line); line.clear())
+      if (h_.count(stoi(line)) == 0) h_[stoi(line)] = map<int, string>();
+    for (string line; getline(relations, line); line.clear()) {
+      vector<string> v = Split(line, ',');
+      h_[stoi(v[0])][stoi(v[1])] = v[2];
+      h_[stoi(v[1])][stoi(v[0])] = v[2];
+    }
+  }
   IlliniBook(const IlliniBook& rhs) = delete;
   IlliniBook& operator=(const IlliniBook& rhs) = delete;
   ~IlliniBook() = default;
-  bool AreRelated(int uin_1, int uin_2);
-  bool AreRelated(int uin_1, int uin_2, const std::string& relationship);
-  int GetRelated(int uin_1, int uin_2);
-  int GetRelated(int uin_1, int uin_2, const std::string& relationship);
-  std::vector<int> GetSteps(int uin, int n);
-  size_t CountGroups();
-  size_t CountGroups(const std::string& relationship);
-  size_t CountGroups(const std::vector<std::string>& relationships);
+  bool AreRelated(int uin_1, int uin_2) {
+    queue<int> q;
+    q.push(uin_1);
+    set<int> visited;
+    visited.insert(uin_1);
+    while (!q.empty()) {
+      int t = q.front();
+      q.pop();
+      if (uin_2 == t) return true;
+      for (auto& tt : h_[t]) {
+        if (visited.count(tt.first) != 0) continue;
+        visited.insert(tt.first);
+        q.push(tt.first);
+      }
+    }
+    return false;
+  }
+  bool AreRelated(int uin_1, int uin_2, const std::string& relationship) {
+    queue<int> q;
+    q.push(uin_1);
+    set<int> visited;
+    visited.insert(uin_1);
+    while (!q.empty()) {
+      int t = q.front();
+      q.pop();
+      if (uin_2 == t) return true;
+      for (auto& tt : h_[t]) {
+        if (visited.count(tt.first) != 0 || tt.second != relationship) continue;
+        visited.insert(tt.first);
+        q.push(tt.first);
+      }
+    }
+    return false;
+  }
+  int GetRelated(int uin_1, int uin_2) {
+    queue<int> q;
+    q.push(uin_1);
+    set<int> visited;
+    visited.insert(uin_1);
+    int cnt = 0;
+    while (!q.empty()) {
+      for (unsigned c = q.size(); c > 0; --c) {
+        int t = q.front();
+        q.pop();
+        if (uin_2 == t) return cnt;
+        for (auto& tt : h_[t]) {
+          if (visited.count(tt.first) != 0) continue;
+          visited.insert(tt.first);
+          q.push(tt.first);
+        }
+      }
+      cnt++;
+    }
+    return -1;
+  }
+  int GetRelated(int uin_1, int uin_2, const std::string& relationship) {
+    queue<int> q;
+    q.push(uin_1);
+    set<int> visited;
+    visited.insert(uin_1);
+    int cnt = 0;
+    while (!q.empty()) {
+      for (unsigned c = q.size(); c > 0; --c) {
+        int t = q.front();
+        q.pop();
+        if (uin_2 == t) return cnt;
+        for (auto& tt : h_[t]) {
+          if (visited.count(tt.first) != 0 || tt.second != relationship)
+            continue;
+          visited.insert(tt.first);
+          q.push(tt.first);
+        }
+      }
+      cnt++;
+    }
+    return -1;
+  }
+  std::vector<int> GetSteps(int uin, int n) {
+    queue<int> q;
+    q.push(uin);
+    set<int> visited;
+    visited.insert(uin);
+    for (; n > 0; --n) {
+      for (unsigned c = q.size(); c > 0; --c) {
+        int t = q.front();
+        q.pop();
+        for (auto& tt : h_[t]) {
+          if (visited.count(tt.first) != 0) continue;
+          visited.insert(tt.first);
+          q.push(tt.first);
+        }
+      }
+    }
+    vector<int> ret;
+    while (!q.empty()) ret.push_back(q.front()), q.pop();
+    return ret;
+  }
+  size_t CountGroups() {
+    set<int> visited;
+    size_t cnt = 0;
+    for (auto& i : h_) {
+      if (visited.count(i.first) != 0) continue;
+      queue<int> q;
+      q.push(i.first);
+      cnt++;
+      while (!q.empty()) {
+        int t = q.front();
+        q.pop();
+        for (auto& tt : h_[t]) {
+          if (visited.count(tt.first) != 0) continue;
+          visited.insert(tt.first);
+          q.push(tt.first);
+        }
+      }
+    }
+    return cnt;
+  }
+  size_t CountGroups(const std::string& relationship) {
+    set<int> visited;
+    size_t cnt = 0;
+    for (auto& i : h_) {
+      if (visited.count(i.first) != 0) continue;
+      queue<int> q;
+      q.push(i.first);
+      visited.insert(i.first);
+      cnt++;
+      while (!q.empty()) {
+        int t = q.front();
+        q.pop();
+        for (auto& [x, y] : h_[t]) {
+          if (visited.count(x) != 0 || y != relationship) continue;
+          visited.insert(x);
+          q.push(x);
+        }
+      }
+    }
+    return cnt;
+  }
+  size_t CountGroups(const std::vector<std::string>& relationships) {
+    set<string> relationship;
+    for (const auto& i : relationships) relationship.insert(i);
+    set<int> visited;
+    size_t cnt = 0;
+    for (auto& i : h_) {
+      if (visited.count(i.first) != 0) continue;
+      queue<int> q;
+      q.push(i.first);
+      visited.insert(i.first);
+      cnt++;
+      while (!q.empty()) {
+        int t = q.front();
+        q.pop();
+        for (auto& tt : h_[t]) {
+          if (visited.count(tt.first) != 0 ||
+              relationship.count(tt.second) == 0)
+            continue;
+          visited.insert(tt.first);
+          q.push(tt.first);
+        }
+      }
+    }
+    return cnt;
+  }
+
+private:
+  map<int, map<int, string>> h_;
 };
 
 #endif
